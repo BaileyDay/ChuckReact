57c029e0c630c4da7bd18c8bdc44bb80a719c080
Bailey Day <bailey.j.day@gmail.com>
2020-03-24 13:53:49 -0400

    Initial commit, rewrote code from codesandbox to convert chuck to react

---
+++ src/Components/category.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class category extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <label>
+                        Select Category
+            <select>
+                            <option>Test</option>
+                        </select>
+                    </label>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default category;
---
+++ src/Components/getquote.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class getQuote extends Component {
+    render() {
+        return (
+            <div>
+                <button type="submit">Get Quote</button>
+                <button type="submit">Refresh</button>
+            </div>
+        );
+    }
+}
+
+export default getQuote;
